40184bdf80e0b95b30a35e519e4eef31abd53723
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2021-01-10 16:43:37 +0200

    add multilevel and edit process

---
+++ src/MultiLevel.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Queue;
+
+/*
+    RR
+FCFS
+*/
+public class MultiLevel {
+    ArrayList<process> processes;
+    int currentTime;
+
+    MultiLevel(ArrayList<process> processes){
+        this.processes = processes;
+        this.currentTime = 0;
+
+    }
+
+}
